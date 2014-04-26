ee09162f7719159560ad4fe53cf145b49d83e6b4
Daniel <daniel.langemann@gmx.de>
2014-04-26 13:10:16 +0200

    added composer

---
+++ config.rb
@@ -0,0 +1,13 @@
+require 'bootstrap-sass'
+
+http_path = "/"
+css_dir = "web/css"
+sass_dir = "sass"
+images_dir = "img"
+javascripts_dir = "web/js"
+
+output_style = :compressed
+
+# To enable relative paths to assets via compass helper functions. Uncomment:
+# relative_assets = true
+line_comments = false
